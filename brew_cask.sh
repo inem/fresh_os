35e74547f8732d999b1d8a0871eef8b3f6f56885
Ivan Nemytchenko <van.omsk@gmail.com>
2018-02-16 17:28:30 +0100

    Create brew_cask.sh

---
+++ brew_cask.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+
+# install homebrew: https://brew.sh/
+
+brew tap caskroom/cask
+
+# must have apps
+
+brew cask install spotify
+brew cask install spotifree
+brew cask install sublime-text
+
+brew cask install typora
+
+brew cask install opera
+
+brew cask install bartender
+brew cask install google-chrome
+brew cask install odrive
+brew cask install evernote
+brew cask install rocket
+brew cask install rescuetime
+brew cask install vox
+brew cask install choosy
+brew cask install alfred
+brew cask install google-photos-backup-and-sync
+brew cask install the-unarchiver
+
+brew cask install toggl
+brew cask install textbar  # warning, paid version probably
+brew cask install zoomus
+brew cask install webtorrent
+brew cask install smcfancontrol
+brew cask install sip
+brew cask install busycal
+brew cask install busycontacts
+brew cask install carbon-copy-cloner
+
+
+brew cask install ealeksandrov-cd-to
+
+brew cask install itsycal
+
+
+
+
+
+
+
+brew cask install sketch
+brew cask install skitch
+brew cask install licecap
+brew cask install handbrake
+
+brew cask install gitx
+brew cask install gitbox
+
+brew cask install imageoptim
+
+
+brew cask install keyboard-cleaner
+brew cask install sketchpacks
+
+brew cask install telegram-desktop
+brew cask install telegram
+
+brew cask install skype
+
+
+
+brew cask install vlc
+
+
+
+
+brew cask install bettertouchtool
+brew cask install karabiner-elements
+
+brew cask install 1password
+brew cask install 1password-cli
+brew cask install beardedspice
+
+
+brew cask install scapple
+
+
+
+
+# dev tools
+
+brew cask install atom
+brew cask install virtualbox
+brew cask install electron
+brew cask install docker
+brew cask install psequel
+
+
+
+
+
+
+brew cask install steam
+
